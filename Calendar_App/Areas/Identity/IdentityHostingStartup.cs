94a2680336ba26ce97a38c2b29bf21041b995c4a
Matt Bauman <mattbauman1@gmail.com>
2020-01-22 22:47:44 -0600

    Add project files.

---
+++ Calendar_App/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Calendar_App.Areas.Identity.Data;
+using Calendar_App.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Calendar_App.Areas.Identity.IdentityHostingStartup))]
+namespace Calendar_App.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<Calendar_AppContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("Calendar_AppContextConnection")));
+
+                services.AddDefaultIdentity<Calendar_AppUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<Calendar_AppContext>();
+            });
+        }
+    }
+}
---
+++ Calendar_App/Migrations/20200123042020_UserUpdate.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Calendar_App.Migrations
+{
+    public partial class UserUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "AspNetUsers");
+        }
+    }
+}
